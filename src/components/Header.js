960428d4f4ab27d26701a28442bcef038be0ba5a
ricardo.machado <ricardo.machado@paygo-consult.com.br>
2023-06-20 16:10:38 -0300

    add footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ const HeaderContainer = styled.header`
 export default function Header(){
     return (
         <HeaderContainer className="App-header">
-          <h1><span role="img" aria-label="Calculate your Pace">⏱️</span> Calculate your Pace!</h1>
+          <h1><span role="img" aria-label="Calculate your Pace">⏱️</span> Calcule o seu Pace!</h1>
         </HeaderContainer>
     );
 }
